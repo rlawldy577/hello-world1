8dcbda5046c7f9175a362bdf1b70cecae99eac8c
Jiyong <rlawldyd577@daum.net>
2017-11-15 00:04:32 +0900

    Add 'Add','Sub','Mul','Div' functions

--- Calculator.c
+++ Calculator.c
@@ -5,3 +5,20 @@ int main()
   printf("This is a basic calculator");
   return 0;
 }
+int Add(int x, int y) {
+  return x+y;
+}
+int Sub(int x, int y) {
+  return x-y;
+}
+int Mul(int x, int y) {
+  return x*y;
+}
+int Div(int x, int y) {
+  if(y == 0) {
+    return 0;
+  }
+  else {
+    return x/y;
+  }
+}
